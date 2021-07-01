9df54ddc10e98230f720e2908c93c80d25467fa4
Galina-Baeva <galina.baeva@sperasoft.com>
2021-07-01 20:39:20 +0300

    Binary search added

--- src/SecondTask.java
+++ src/SecondTask.java
@@ -16,6 +16,8 @@ public class SecondTask {
         setRandomArray(a, arr);
         System.out.println(Arrays.toString(arr));
         System.out.println(findNumberInArray(a, b, arr));
+        bubbleSort(a, arr);
+        System.out.println(findNumberBinary(a, b, arr));
     }
 
     public static void setRandomArray(int a, int[] arr) {
@@ -37,13 +39,34 @@ public class SecondTask {
     }
 
     public static boolean findNumberBinary(int a, int b, int[] arr) {
-        for (int i = 0; i < a; i++) {
-            if (arr[i] == b) {
+        int start = 0;
+        int end = a - 1;
+        while (!(start >= end)) {
+            int c = start + (end - start) / 2;
+            if (arr[c] == b) {
                 return true;
-            } else if (i == a - 1) {
-                return false;
+            } else if (arr[c] > b) {
+                end = c;
+            } else {
+                start = c + 1;
             }
         }
         return false;
+    }
+
+    public static void bubbleSort(int a, int[] arr) {
+        boolean sort = false;
+        int b;
+        while (!sort) {
+            sort = true;
+            for (int i = 0; i < a - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    sort = false;
+                    b = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = b;
+                }
+            }
+        }
     }
 }
